9cf9ee7f7e91e6e6eaa396ebd961efd4446306f3
ADHIKSHA <adhikshat1905@gmail.com>
2021-05-29 19:50:27 +0530

    first commit

--- test.py
+++ test.py
@@ -15,7 +15,8 @@ def remove_duplicates(input: list) -> list:
         List without duplicates.
     """
     no_dup_dict = dict.fromkeys(input)
-    return no_dup_dct
+    no_dup_list= list(no_dup_dict.keys())
+    return no_dup_list
 
 
 def sort_list(input: list) -> list:
@@ -30,6 +31,7 @@ def sort_list(input: list) -> list:
     Returns:
         Sorted list.
     """
+    input.sort()
     return input
 
 
